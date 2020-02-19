cf8d89ea6da353a3e1467d34a25fd012de99ba24
eslameltaher <eslam.eltaher@hotmail.com>
2020-02-19 09:31:19 +0200

    added Migrations

---
+++ EmployeeSystem.Persistence/Migrations/20200219071815_InitialModel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeSystem.Persistence.Migrations
+{
+    public partial class InitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Countries",
+                columns: table => new
+                {
+                    CountryID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CountryName = table.Column<string>(maxLength: 50, nullable: true),
+                    CountryCode = table.Column<string>(maxLength: 2, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Countries", x => x.CountryID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employees",
+                columns: table => new
+                {
+                    empID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    empName = table.Column<string>(maxLength: 50, nullable: true),
+                    empTitle = table.Column<string>(maxLength: 50, nullable: true),
+                    empSalary = table.Column<float>(nullable: false),
+                    empEmail = table.Column<string>(maxLength: 50, nullable: true),
+                    empBirthDate = table.Column<DateTime>(nullable: false),
+                    empPhoto = table.Column<byte[]>(nullable: true),
+                    CountryID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employees", x => x.empID);
+                    table.ForeignKey(
+                        name: "FK_Employees_Countries_CountryID",
+                        column: x => x.CountryID,
+                        principalTable: "Countries",
+                        principalColumn: "CountryID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_CountryID",
+                table: "Employees",
+                column: "CountryID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "Countries");
+        }
+    }
+}
