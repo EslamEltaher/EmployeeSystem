6d6c281eb0e66b2a17da6a34bb999860898c83e7
eslameltaher <eslam.eltaher@hotmail.com>
2020-02-19 09:09:23 +0200

    added EntityConfigurations in Context & added Nuget Package EntityFrameworkCore.Relational

--- EmployeeSystem.Persistence/EmployeeContext.cs
+++ EmployeeSystem.Persistence/EmployeeContext.cs
@@ -10,5 +10,72 @@ namespace EmployeeSystem.Persistence
     {
         public DbSet<Employee> Employees { get; set; }
         public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            /*
+                empID int
+                empName nchar(50)
+                empTitle nchar(50)
+                empSalary float
+                empEmail nchar(50)
+                empBirthDate date
+                empPhoto varbinary(MAX)
+                CountryID int
+            */
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Id)
+                .HasColumnName("empID");
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Name)
+                .HasColumnName("empName")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Title)
+                .HasColumnName("empTitle")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Salary)
+                .HasColumnName("empSalary");
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Email)
+                .HasColumnName("empEmail")
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.BirthDate)
+                .HasColumnName("empBirthDate");
+
+            modelBuilder.Entity<Employee>()
+                .Property(e => e.Photo)
+                .HasColumnName("empPhoto");
+
+
+            /*
+                CountryID int
+                CountryName nchar(50)
+                CountryCode Ncahr(2)
+             */
+            modelBuilder.Entity<Country>()
+               .Property(c => c.Id)
+               .HasColumnName("CountryID");
+
+            modelBuilder.Entity<Country>()
+               .Property(c => c.CountryName)
+               .HasColumnName("CountryName")
+               .HasMaxLength(50);
+
+            modelBuilder.Entity<Country>()
+                .Property(c => c.CountryCode)
+                .HasColumnName("CountryCode")
+                .HasMaxLength(2);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
